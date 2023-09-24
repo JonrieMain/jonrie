a069bffb7796c1bb9207ff2918542e7b1043abfb
Jonrie Morales Badiang <jonriemain@gmail.com>
2023-09-24 08:15:52 -0400

    add date to project/add 2 testi

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -198,83 +198,112 @@ const About = ()=>{
                 <div id="scroll">
     
                     <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">5/24/2022</div>
                         <span>
                             <i className="fa-solid fa-user"></i>
                         </span>
-                        <h3><a target="_blank" href="https://web.facebook.com/stevefrancis.galo.1"><i className="fa-brands fa-facebook"></i></a> Steve Francis Samaco <small>(Student)</small></h3>
+                        <h3><a target="_blank" href="https://web.facebook.com/stevefrancis.galo.1"><i className="fa-brands fa-facebook"></i></a> Steve Francis Samaco <small>(ICT Student)</small></h3>
                         <p>Jonrie is a very good person and very helpful; there's a lot he's done to help me, like those projects, quizzes, and modules about programming; if I don't understand, Jonrie is there to teach me; Jonrie is very nice; I am speechless; I hope he becomes successful someday.</p>
                     </div>
     
                     <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">5/28/2022</div>
                         <span>
                             <i className="fa-solid fa-user"></i>
                         </span>
-                        <h3><a target="_blank" href="https://web.facebook.com/rainier.barredo24"><i className="fa-brands fa-facebook"></i></a> Rainier Barredo <small>(Student)</small></h3>
+                        <h3><a target="_blank" href="https://web.facebook.com/rainier.barredo24"><i className="fa-brands fa-facebook"></i></a> Rainier Barredo <small>(ICT Student)</small></h3>
                         <p>Jonrie Morales is very good in terms of programming. He helped me before to build a web-based resume, and I recommend that you ask him for help because he is reliable and he is such a nice person.</p>
                     </div>
     
                     <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">6/1/2022</div>
                         <span>
                             <i className="fa-solid fa-user"></i>
                         </span>
-                        <h3><a target="_blank" href="https://web.facebook.com/profile.php?id=100078333846946"><i className="fa-brands fa-facebook"></i></a> Lheymar Catapang <small>(Student)</small></h3>
+                        <h3><a target="_blank" href="https://web.facebook.com/profile.php?id=100078333846946"><i className="fa-brands fa-facebook"></i></a> Lheymar Catapang <small>(ICT Student)</small></h3>
                         <p>Jonrie is one of my friends. We are not really close, but I feel good about him helping me with other schoolwork, especially in computer programming, about which I have no knowledge. When I chatted with him, he immediately helped me build the blog site about my school. For me jonrie is very nice person at realible.</p>
                     </div>
     
                     <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">12/13/2022</div>
                         <span>
                             <i className="fa-solid fa-user"></i>
                         </span>
-                        <h3><a target="_blank" href="https://web.facebook.com/brecio.charlesfrancis"><i className="fa-brands fa-facebook"></i></a> Charles Francis <small>(Student)</small></h3>
+                        <h3><a target="_blank" href="https://web.facebook.com/brecio.charlesfrancis"><i className="fa-brands fa-facebook"></i></a> Charles Francis <br /><small>(BSIT Student)</small></h3>
                         <p>Jonrie taught me programming since we were in Junior High School. Now that we are in college, he still teaches me new knowledge he gain. He is knowledgeable in web development-- he can do both front end and back end. As far as I know him, his skills includes: HTML, CSS, JavaScript, phpMyAdmin, MySQL and C++. He is very enthusiast, passionate, and hardworking person. I wish him the best and have the success he deserves.</p>
                     </div> 
-    
+
+
                     <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">1/11/2023</div>
                         <span>
                             <i className="fa-solid fa-user"></i>
                         </span>
-                        <h3><a target="_blank" href="https://web.facebook.com/profile.php?id=100078759327384"><i className="fa-brands fa-facebook"></i></a> Melissa Ayala <small>(Student)</small></h3>
+                        <h3><a target="_blank" href="https://web.facebook.com/jeromee.dev.22"><i className="fa-brands fa-facebook"></i></a> Jerome Bulosan <br /><small>(BSIT Student)</small></h3>
+                        <p>For me, the amount of help that Jonrie did for us was so great that we could present something in defense; you did a great job. Your skill defines how good you are in your field. I hope you can improve and do more projects. I will highly recommend you to others who are looking to build their systems. The work is beautiful, and the system is smooth and well organized. Thank you</p>
+                    </div> 
+
+    
+                    <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">4/9/2023</div>
+                        <span>
+                            <i className="fa-solid fa-user"></i>
+                        </span>
+                        <h3><a target="_blank" href="https://web.facebook.com/profile.php?id=100078759327384"><i className="fa-brands fa-facebook"></i></a> Melissa Ayala <br /> <small>(BSIT Student)</small></h3>
                         <p>All I can say is that Jonrie's help with part of my project is a big help to me because I'm a bit weak in making codes. So thank you for helping me with my project; the amount of work I have to do has been reduced at least a little bit. I can say that you are nice to talk to, and you also responded right away, which was nice, and the help I got from you followed the layout of my project. Thank you.</p>
                     </div> 
     
-                    <div data-aos="fade-up" data-aos-duration="1000">
-                        <span>
-                            <i className="fa-solid fa-user"></i>
-                        </span>
-                        <h3><a target="_blank" href="https://web.facebook.com/jeromee.dev.22"><i className="fa-brands fa-facebook"></i></a> Jerome Bulosan <small>(Student)</small></h3>
-                        <p>For me, the amount of help that Jonrie did for us was so great that we could present something in defense; you did a great job. Your skill defines how good you are in your field. I hope you can improve and do more projects. I will highly recommend you to others who are looking to build their systems. The work is beautiful, and the system is smooth and well organized. Thank you</p>
-                    </div> 
     
                     <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">5/25/2023</div>
                         <span>
                             <i className="fa-solid fa-user"></i>
                         </span>
-                        <h3><a target="_blank" href="https://web.facebook.com/kristine.Dingle.04"><i className="fa-brands fa-facebook"></i></a> Kristine Ocan Dingle <small>(Student)</small></h3>
+                        <h3><a target="_blank" href="https://web.facebook.com/kristine.Dingle.04"><i className="fa-brands fa-facebook"></i></a> Kristine Ocan Dingle <br /><small>(BSIT Student)</small></h3>
                         <p>Im glad that I saw your content and all I can say is you're so kind that you help me to do my codes/activity you help me to learn more about coding and you're so informative and explaining all the details also you are easy to approach because you don't hesitate to message me back. Once again Thank you for you help I appreciate it.
                         </p>
                     </div> 
 
                     <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">9/1/2023</div>
                         <span>
                             <i className="fa-solid fa-user"></i>
                         </span>
-                        <h3><a target="_blank" href="https://www.facebook.com/profile.php?id=100081368501361"><i className="fa-brands fa-facebook"></i></a>Nathaniel Bianito <small>(Student)</small></h3>
+                        <h3><a target="_blank" href="https://www.facebook.com/profile.php?id=100081368501361"><i className="fa-brands fa-facebook"></i></a>Nathaniel Bianito <br /> <small>(BSIT Student)</small></h3>
                         <p>I recently commissioned from jonrie and I am absolutely thrilled with the results. From start to finish, the experience was exceptional.</p>
                     </div> 
 
 
                     <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">9/14/2023</div>
                         <span>
                             <i className="fa-solid fa-user"></i>
                         </span>
-                        <h3><a target="_blank" href="https://web.facebook.com/claryellaa"><i className="fa-brands fa-facebook"></i></a>Scarlett Soledad <small>(Student)</small></h3>
+                        <h3><a target="_blank" href="https://web.facebook.com/claryellaa"><i className="fa-brands fa-facebook"></i></a>Scarlett Soledad <br /> <small>(BSIT Student)</small></h3>
                         <p>Super accommodating and reliable! And the work is beautiful and easy to talk about. The price is very budget-friendly, which is very helpful for students.</p>
                     </div> 
 
+                    <div data-aos="fade-up" data-aos-duration="1000">
+                    <div className="date">9/24/2023</div>
+                        <span>
+                            <i className="fa-solid fa-user"></i>
+                        </span>
+                        <h3><a target="_blank" href="https://web.facebook.com/jolinaagojo.18"><i className="fa-brands fa-facebook"></i></a>Jolina Agojo <br /><small>(BSCS Student)</small></h3>
+                        <p>As a student who hired a web developer to make my website, it is nice and well made, very responsive, and very nice to talk to.</p>
+                    </div> 
 
 
+                    <div data-aos="fade-up" data-aos-duration="1000">
+                        <div className="date">9/24/2023</div>
+                        <span>
+                            <i className="fa-solid fa-user"></i>
+                        </span>
+                        <h3><a target="_blank" href="https://web.facebook.com/hannah.dimayuga.1"><i className="fa-brands fa-facebook"></i></a>Hannah Isabel Dimayuga <br /> <small>(BSCS Student)</small></h3>
+                        <p>Jonrie is very professional, and he easily approaches and builds my webpage. Aside from that, his work is so fine.</p>
+                    </div> 
 
         
+
     
                 </div>
     
